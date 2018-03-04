5869a0d2163cc225f7d69bd050e1d22a7acfb220
Marko Poikkimäki <markopoikkimaki@Markos-MBP.lan>
2018-03-04 11:14:12 +0100

    ItemCell & TableView

---
+++ View/ItemCell.swift
@@ -0,0 +1,30 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Marko Poikkimäki on 2018-03-04.
+//  Copyright © 2018 Marko Poikkimäki. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
